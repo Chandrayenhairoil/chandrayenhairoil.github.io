f8777119a2e3904330bb35c0b9d04ad4d35c5be1
Chandrayenhairoil <77076645+Chandrayenhairoil@users.noreply.github.com>
2021-01-07 11:51:51 +0530

    Update email.php

--- email.php
+++ email.php
@@ -5,13 +5,13 @@
   $phonenumber=$_POST['phonenumber'];
   $residentialaddress=$_POST['residentialaddress'];
   
-  $to ='vrkrock7@gmail.com'
+  $to='vrkrock7@gmail.com'
   $subject='New Form Submission';
   $message="Name: ".$name"\n"."Phone: ".$phone."\n". "Residentialaddress: "."\n\n".$residentialaddress;
   $headers="From: ".$email;
 
   if(mail($to, $subject, $message, $headers)){
-     echo"<h1>Submitted successfully! Thank you"." ".$name.",We will cotact you later</h1>";
+     echo "<h1>Submitted successfully! Thank you"." ".$name.",We will cotact you later</h1>";
   }
   else{
     echo "oops! Something went wrong!";
