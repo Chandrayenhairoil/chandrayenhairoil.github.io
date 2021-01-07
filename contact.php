99f83add70e85017a473b0a9db124b3e7768fa38
Chandrayenhairoil <77076645+Chandrayenhairoil@users.noreply.github.com>
2021-01-07 10:14:26 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+  $name = $_POST['name']
+  $visitor_email = $_POST['email']
+  $phonenumber = $_POST['phonenumber']
+  $residentialaddress = $_POST['residentialaddress']
+  
+  $email_from = 'chandrayenhairoil.github.io';
+  $email_subject = 'New Form Submission';
+  $email_body = "User Name: $name.\n"
+                    "User Email: $visitor_email.\n".
+                    "User Phonenumber: $phonenumber.\n".
+                    "User Residentaladdress: $residentaladdress\n";
+
+  $to ="vrkrock7@gmail.com"                   
+  
+  $headers ="From: $email_from \r\n"
+
+  $headers ="Reply-To: $visitor_from \r\n"
+
+  mail($to,$email_subject,$email_body,$headers);
+
+  header("Location: form.html");
+  
+?>
